5b124e21bdadb83d2df33128076bcff3295df5a9
Anton Tsukanov <antonpower2101@gmail.com>
2025-08-10 19:50:36 +0300

    Исправление отображения

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
         <div className='w-screen overflow-hidden'>
           <Header />
           <main className='mt-20 w-full max-w-[1080px] mx-auto min-h-[calc(100vh-205px)] px-4 py-5 relative'>
-            <section className='absolute -z-10 -top-[4%] -left-[150%] sm:-left-[75%] -rotate-45'>
-              <div className='bg-white opacity-[2%] h-96 w-[200vw]'></div>
-              <div className='bg-white opacity-[2%] h-12 w-[200vw] mt-5'></div>
+            <section className='absolute -z-1 -top-[4%] -left-[150%] sm:-left-[75%]  -rotate-45'>
+              <div className='bg-white opacity-[2%] h-96 w-4xl'></div>
+              <div className='bg-white opacity-[2%] h-12 w-4xl mt-5'></div>
             </section>
             {children}
           </main>
