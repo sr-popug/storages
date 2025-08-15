e49e9dacf2ef1247aa45199ef86df25da3edc052
Anton Tsukanov <antonpower2101@gmail.com>
2025-08-15 17:25:25 +0300

    Испрвления

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className='mx-auto mt-15 max-w-[1080px] flex py-5 px-2 gap-12 justify-between items-center border-t-1 border-neutral-800'>
+    <footer className='mx-auto mt-15 max-w-[1080px] flex py-3 sm:py-5 px-3 sm:px-2 gap-12 justify-between items-center border-t-1 border-neutral-800'>
       <Link
-        className='underline text-[10px] sm:text-sm text-neutral-600'
+        className='underline text-[12px] sm:text-sm text-neutral-600'
         target='_blank'
         href='/user-agreement'
       >
         Пользовательское соглашение
       </Link>
 
-      <p className='text-[10px] sm:text-sm text-neutral-600'>
+      <p className='text-[12px] sm:text-sm text-neutral-600'>
         © 2025. Все права защищены{" "}
       </p>
     </footer>
